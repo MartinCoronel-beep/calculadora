0cdedc8d4612243a66f80c28e504bbecd406f735
MartinCoronel-beep <ma.coronel@duocuc.cl>
2024-06-24 12:32:13 -0400

    cambios

--- calculadora.py
+++ calculadora.py
@@ -3,7 +3,7 @@ num2 = int(input("numero 2: "))
 num3 = int(input("numero 3: ")) 
 
 valor = 0
-while True:
+while valor != 6:
     print("""seleccione opcion
             1- Sumar 
             2- Restar
@@ -16,19 +16,15 @@ while True:
 
     if valor == 1:
         print("la suma es",num1+num2)
-        break;
     if valor == 2:
         print("la resta es",num1-num2)
-        break;
     if valor == 3:
         print("la multiplicacion es",num1*num2)
-        break;
     if valor == 4:
         print("la division es",num1/num2)
-        break;
     if valor == 5:
         print("la suma es",num1+num2+num3)
-        break;
+    if valor == 6:
+       break
     else:
-        print("Opcion incorrecta")
-        break;
+        print("Opcion incorrecta ")
